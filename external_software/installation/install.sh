6056629619e71f7010b38521151045c2546cf767
josruirod <joseluis.ruiz@ebd.csic.es>
2023-10-31 10:43:17 +0100

    Installation of external software

--- external_software/installation/install.sh
+++ external_software/installation/install.sh
@@ -82,9 +82,9 @@ echo -e "\nSmall fix on check_strandedness...\n"
 cd $(dirname $(find $conda_envs_path/reanalyzerGSE_4 -name check_strandedness.py))
 rm check_strandedness.py; wget -q https://github.com/signalbash/how_are_we_stranded_here/raw/master/how_are_we_stranded_here/check_strandedness.py; chmod 775 check_strandedness.py
 
-echo -e "\n\nInstalling the CRAN's package 'autoGO' and some dependencies, not available through conda as of yet... This manual installation implies that the version of the installed R packages are not frozen. So, not likely, but please do keep in mind that this may be a source of errors in the mid-term if newer versions of the R packages are installed. As of the moment of the writing of this wrapper script, the version of autoGO being installed was 0.9.1...\n\n"
+echo -e "\n\nInstalling the CRAN's package 'autoGO' and some other dependencies, not available through conda as of yet... This manual installation implies that the version of the installed R packages are not frozen. So, not likely, but please do keep in mind that this may be a source of errors in the mid-term if newer versions of the R packages are installed. As of the moment of the writing of this wrapper script, for example the version of autoGO being installed was 0.9.1...\n\n"
 export PATH=$conda_envs_path/reanalyzerGSE_3/bin:$PATH
-$conda_envs_path/reanalyzerGSE_3/bin/Rscript -e 'suppressMessages({install.packages("autoGO",repos="https://cloud.r-project.org",quiet=T)})' &> /dev/null # Avoid the very extensive logs being printed out, please remove suppressMessages and "&> /dev/null" to double check if installation fails... 
+$conda_envs_path/reanalyzerGSE_3/bin/Rscript -e 'suppressMessages({BiocManager::install(c("ComplexHeatmap","DESeq2","GSVA"),update=F,ask=F)});suppressMessages({install.packages(c("autoGO","rentrez"),repos="https://cloud.r-project.org",quiet=T)})' &> /dev/null # Avoid the very extensive logs being printed out, please remove suppressMessages and "&> /dev/null" to double check if installation fails...
 
 # https://github.com/DerrickWood/kraken2/issues/518, you just have to manually replace 'ftp' by 'https' in the line 46 of the file 'rsync_from_ncbi.pl'
 # Fix and improve kraken2 2.1.2 within conda (i.e. fix download for database building and improve masking with parallel):
