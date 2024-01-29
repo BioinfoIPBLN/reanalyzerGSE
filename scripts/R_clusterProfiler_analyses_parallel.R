4b9066d02d225f080b3711894aec5403cf855253
josruirod <joseluis.ruiz@ebd.csic.es>
2024-01-29 13:47:54 +0100

    Scripts

--- scripts/R_clusterProfiler_analyses_parallel.R
+++ scripts/R_clusterProfiler_analyses_parallel.R
@@ -1025,6 +1025,7 @@ print("Tidying...")
 removeEmptyDirs <- function(directory) {
   # List all directories
   dirs <- list.dirs(directory, recursive = TRUE)
+  dirs <- dirs[grep("funct",dirs)]
   
   # Check each directory
   for (fold in dirs) {
