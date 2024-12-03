edc140715f0a0cff13c35bc19eb41c588068b7f5
josruirod <joseluis.ruiz@ebd.csic.es>
2024-12-03 13:04:40 +0100

    Scripts

--- scripts/manual_options.sh
+++ scripts/manual_options.sh
@@ -18,18 +18,18 @@ databases_function=GO_Biological_Process_2023,GO_Molecular_Function_2023,GO_Cell
 #### Metadata and sample info:
 design_custom_local=
 design_custom=no
-organism_argument=
+organism=
 taxonid=
-number_reads=
+number_reads_to_subsample=
 batch_vector=
 batch_biological_covariates=
 covariables=none
-differential_expr_comparisons=no
 target=
 
 #### Activate alternative modes:
+debug_step=all
 qc_raw_reads=yes
-perform_differential_analyses=yes
+full_differential_analyses=yes
 functional_enrichment_analyses=yes
 clusterProfiler_full=no
 batch=no
@@ -42,7 +42,6 @@ convert_tables_excel=no
 time_course=no
 network_analyses=no
 auto_panther_log=no
-debug_step=all
 
 #### Processing parameters:
 strand=
@@ -54,13 +53,14 @@ differential_expr_soft=edgeR
 fastp_mode=no
 fastp_adapter=no
 fastp_trimmnig=none
-minstd=1
-mestimate=0
+time_course_std=1
+time_course_fuzz=0
 
-#### Filtering out samples:
+#### Filtering out samples/comparisons:
 GSM_filter=
 stop=no
 pattern_to_remove=none
+differential_expr_comparisons=no
 
 #### Functional enrichment/networking analyses
 clusterProfiler_method=fdr
@@ -68,12 +68,12 @@ clusterProfiler_universe=detected
 clusterProfiler_minGSSize=10
 clusterProfiler_maxGSSize=500
 panther_method=FDR
-rev_thr=0.7
+revigo_threshold_similarity=0.7
 
 #### Performance:
 cores=30
 cores_reads_to_subsample=10
-indexthreads=30
+cores_index=30
 memory_max=257698037760
 number_parallel=10
 compression_level=9
