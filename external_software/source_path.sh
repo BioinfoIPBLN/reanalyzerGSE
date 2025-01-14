347f1db0f368b504658f8474d4919dfbc96a2c52
josruirod <joseluis.ruiz@ebd.csic.es>
2025-01-14 18:22:38 +0100

    PATH and variables to export

--- external_software/source_path.sh
+++ external_software/source_path.sh
@@ -6,13 +6,12 @@ type conda >/dev/null 2>&1 || { echo >&2 "Conda is required to be installed... a
 
 if [ "$conda_within_reanalyzer" == "yes" ]; then	
 	conda_dir=$EXTERNAL_SOFTWARE_DIR/miniconda3/bin
-	conda_exec=$($EXTERNAL_SOFTWARE_DIR/miniconda3/bin/conda env list | grep -v "#" | head -1 | sed 's,.* ,,g;s,/envs.*,,g')/bin
+	conda_exec=$($EXTERNAL_SOFTWARE_DIR/miniconda3/bin/conda env list | sed '1d' | grep -v "#" | head -1 | sed 's,.* ,,g;s,/envs.*,,g')/bin
 else	
 	conda_exec=$(conda env list | egrep -v '^#|^$' | head -1 | sed 's,.* ,,g;s,/envs.*,,g')/bin
 	conda_dir=$(echo $conda_exec | sed 's,/bin$,,g;s,/condabin$,,g')	
 fi
 
-conda_exec=$(conda env list | egrep -v '^#|^$' | head -1 | sed 's,.* ,,g;s,/envs.*,,g')/bin
 conda_envs_path=$(echo $conda_exec | sed 's,/bin$,/envs,g;s,/condabin$,/envs,g')
 echo -e "\nDetected conda environments path: $conda_envs_path\n"
 
