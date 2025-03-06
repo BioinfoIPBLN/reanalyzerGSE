d91d4b05e515620d5cc8420b2f51ae34cb95c60e
josruirod <joseluis.ruiz@ebd.csic.es>
2025-03-07 00:41:13 +0100

    Scripts

--- scripts/download_sra_fq.sh
+++ scripts/download_sra_fq.sh
@@ -3,10 +3,11 @@ file=$1
 dest=$2
 number_parallel=$3 
 cores=$4
-compression_level=$5
+#compression_level=$5
 
 total_lines=$(cat $1 | wc -l)
-echo -e "\nProcessing and downloading with fasterq-dump $total_lines files\n"
+#echo -e "\nProcessing and downloading with fasterq-dump $total_lines files\n"
+echo -e "\nProcessing and downloading with fastq-dl $total_lines files\n"
 
 # Make sure cores get distributed:
 if [ $number_parallel -le $cores ]; then
@@ -16,15 +17,17 @@ else
 	cores_parallel=$((cores / number_parallel))
 fi
 
-echo -e "\nUsing parallel blocks of $number_parallel files with $cores_parallel cores each and $compression_level compression level\n"
+#echo -e "\nUsing parallel blocks of $number_parallel files with $cores_parallel cores each and $compression_level compression level\n"
+echo -e "\nUsing parallel blocks of $number_parallel files with $cores_parallel cores\n"
 
 
 # Processing:
 cd $dest
 # https://www.biostars.org/p/359441/
-parallel --verbose --joblog $dest/../download_sra_fq_log_parallel.txt -j $number_parallel download_sra_fq0.sh {} $cores_parallel $compression_level ::: $(cat $file)
+# parallel --verbose --joblog $dest/../download_sra_fq_log_parallel.txt -j $number_parallel download_sra_fq0.sh {} $cores_parallel $compression_level ::: $(cat $file)
 
-
+parallel --verbose --joblog $dest/../download_sra_fq_log_parallel.txt -j $number_parallel fastq-dl --accession {} --cpus $cores_parallel ::: $(cat $file)
+rm fastq-run*
 
 
 
